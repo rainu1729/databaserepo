ef6b0ec31a1ab5b943eb3d19811ec862affc85cb
rainu <rainu1729@users.noreply.github.com>
2020-03-07 10:35:48 +0530

    Update JOBS.sql

--- TABLES/JOBS.sql
+++ TABLES/JOBS.sql
@@ -20,6 +20,7 @@ References with employees and job_history table.';
   ALTER TABLE "HR"."JOBS" MODIFY ("JOB_TITLE" CONSTRAINT "JOB_TITLE_NN" NOT NULL ENABLE);
   ALTER TABLE "HR"."JOBS" ADD CONSTRAINT "JOB_ID_PK" PRIMARY KEY ("JOB_ID")
   USING INDEX  ENABLE;
+  ---
   
   --feature
 
